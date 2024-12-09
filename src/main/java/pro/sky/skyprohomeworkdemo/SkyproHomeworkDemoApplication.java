4cc12e8ae7fa2c78386124c2e1b3590d11a12f61
MrLo-jpg <lsa-71@mail.ru>
2024-12-09 19:55:06 +0300

    init

---
+++ src/main/java/pro/sky/skyprohomeworkdemo/SkyproHomeworkDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohomeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomeworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproHomeworkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprohomeworkdemo/SkyproHomeworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohomeworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHomeworkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
